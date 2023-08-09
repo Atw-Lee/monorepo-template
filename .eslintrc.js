4778169e953409f61aaa0a6bfe573bfd7e239329
lee <atwlee0204@gamil.com>
2023-08-09 12:01:22 +0800

    feat: change app dir & ui package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
+/*
+ * @Author: Do not edit
+ * @Date: 2023-08-08 23:29:57
+ * @LastEditors: atwlee
+ * @LastEditTime: 2023-08-09 11:55:54
+ * @Descripttion: 
+ * @FilePath: /monorepo-template/.eslintrc.js
+ */
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["custom"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/"],
-    },
-  },
 };
---
+++ packages/ui/src/Button/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return <button {...props}>button</button>;
+};
