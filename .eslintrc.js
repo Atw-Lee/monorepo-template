ecffd694a71a4b08b3cf7f526492426417a85f30
lee <atwlee0204@gamil.com>
2023-08-09 12:12:21 +0800

    feat(pck.json): 增加commitizen配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,3 @@
-/*
- * @Author: Do not edit
- * @Date: 2023-08-08 23:29:57
- * @LastEditors: atwlee
- * @LastEditTime: 2023-08-09 11:55:54
- * @Descripttion: 
- * @FilePath: /monorepo-template/.eslintrc.js
- */
 module.exports = {
   root: true,
   extends: ["custom"],
